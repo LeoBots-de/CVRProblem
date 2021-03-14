65b0583e6756a6d8613514a3ebb47d3d65ebd543
Marco Zwillus <mz29zode@studserv.uni-leipzig.de>
2021-03-14 12:44:34 +0100

    create API

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+import json
+import sys
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
+def solveproblem(req):
+
+    return res
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Es funzt!'
+
+
+@app.route('/packaging', methods=['POST'])
+def packaging():
+    answer = solveproblem(request.json)
+    return answer
 
 
 if __name__ == '__main__':
-    app.run()
+
+    if(len(sys.argv) == 2):
+        with open(sys.argv[1]) as json_file:
+            data = json.load(json_file)
+        answer = solveproblem(data)
+        print(answer)
+    else:
+        app.run(host='0.0.0.0')
